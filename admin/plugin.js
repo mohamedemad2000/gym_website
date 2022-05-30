8be9f0a1207ba402b3274cea99d45e870d98e71a
mohamedemad2000 <mohamedemad147@gmail.com>
2022-05-30 15:16:30 +0200

    my web app

---
+++ admin/plugin.js
@@ -0,0 +1,17 @@
+function back(color)
+{
+  document.body.style.background=color;
+}
+function txtcolor(color)
+{
+   document.getElementById("pgfont").style.color=color;
+   document.getElementById("admincolor").style.color=color;
+   document.getElementById("txtcolor1").style.color=color;
+   document.getElementById("txtcolor2").style.color=color;
+   document.getElementById("txtcolor3").style.color=color;
+}
+function txtsize(size)
+{
+    document.getElementById("ad").style.fontSize=size;
+    document.getElementById("pgfont").style.fontSize=size;
+}
---
+++ assest/js/style.js
@@ -0,0 +1,16 @@
+var counter = 1;
+setInterval(function()
+{
+    document.getElementById('radio'+counter).checked=true;
+    counter++;
+    if(counter>4)
+    {
+        counter=1;
+    }
+},5000);
+
+function login()
+{
+    window.location.href="login/login.html";
+}
+
